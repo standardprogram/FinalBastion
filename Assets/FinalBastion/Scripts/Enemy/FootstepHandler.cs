ac37a4595c8c623f8cb15366eb47af7b92dbc6d4
Zhao <standardprogram@foxmail.com>
2016-04-06 21:41:51 +0800

    rewrite some file in c#

---
+++ Assets/FinalBastion/Scripts/Enemy/FootstepHandler.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepHandler : MonoBehaviour {
+	enum FootType {
+		Player,
+		Mech,
+		Spider
+	}
+
+
+	AudioSource audioSource;
+	FootType footType;
+
+	private PhysicMaterial physicMaterial;
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		physicMaterial = collisionInfo.collider.sharedMaterial;
+	}
+
+	void OnFootstep() {
+		if (!audioSource.enabled) {
+			return;
+		}
+
+		AudioClip sound;
+
+		switch (footType) {
+		case FootType.Player:
+			sound = MaterialImpactManager.GetPlayerFootstepSound(physicMaterial);
+			break;
+		case FootType.Mech:
+			sound = MaterialImpactManager.GetMechFootstepSound(physicMaterial);
+			break;
+		case FootType.Spider:
+			sound = MaterialImpactManager.GetSpiderFootstepSound(physicMaterial);
+			break;
+		}
+
+		audioSource.pitch = Random.Range (0.98, 1.02);
+		audioSource.PlayOneShot (sound, Random.Range (0.8, 1.2));
+	}
+
+}
+
+/*
+function OnFootstep () {
+	if (!audioSource.enabled)
+	{
+		return;
+	}
+	
+	var sound : AudioClip;
+	switch (footType) {
+	case FootType.Player:
+		sound = MaterialImpactManager.GetPlayerFootstepSound (physicMaterial);
+		break;
+	case FootType.Mech:
+		sound = MaterialImpactManager.GetMechFootstepSound (physicMaterial);
+		break;
+	case FootType.Spider:
+		sound = MaterialImpactManager.GetSpiderFootstepSound (physicMaterial);
+		break;
+	}	
+	audioSource.pitch = Random.Range (0.98, 1.02);
+	audioSource.PlayOneShot (sound, Random.Range (0.8, 1.2));
+}
+*/
---
+++ Assets/FinalBastion/Scripts/Enemy/MaterialImpactManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class MaterialImpact {
+	PhysicMaterial physicMaterial;
+	AudioClip[] playerFootstepSounds;
+	AudioClip[] mechFootstepSounds;
+	AudioClip[] spiderFootstepSounds;
+	AudioClip[] bulletHitSounds;
+}
+
+
+public class MaterialImpactManager : MonoBehaviour {
+
+	MaterialImpact[] materials;
+	
+	private static Dictionary<PhysicMaterial, MaterialImpact> dict;
+	private static MaterialImpact defaultMat;
+	
+	void Awake () {
+		defaultMat = materials[0];
+		
+		dict = new Dictionary<PhysicMaterial, MaterialImpact> ();
+		for (int i = 0; i < materials.Length; i++) {
+			dict.Add (materials[i].physicMaterial, materials[i]);
+		}
+	}
+	
+	static AudioClip GetPlayerFootstepSound (PhysicMaterial mat) {
+		var imp : MaterialImpact = GetMaterialImpact (mat);
+		return GetRandomSoundFromArray(imp.playerFootstepSounds);
+	}
+	
+	static AudioClip GetMechFootstepSound (PhysicMaterial mat ) {
+		var imp : MaterialImpact = GetMaterialImpact (mat);
+		return GetRandomSoundFromArray(imp.mechFootstepSounds);
+	}
+	
+	static AudioClip GetSpiderFootstepSound (PhysicMaterial mat) {
+		var imp : MaterialImpact = GetMaterialImpact (mat);
+		return GetRandomSoundFromArray(imp.spiderFootstepSounds);
+	}
+	
+	static AudioClip GetBulletHitSound (PhysicMaterial mat) {
+		var imp : MaterialImpact = GetMaterialImpact (mat);
+		return GetRandomSoundFromArray(imp.bulletHitSounds);
+	}
+	
+	static MaterialImpact GetMaterialImpact (PhysicMaterial mat) {
+		if (mat && dict.ContainsKey (mat))
+			return dict[mat];
+		return defaultMat;
+	}
+	
+	static AudioClip GetRandomSoundFromArray (AudioClip[] audioClipArray) {
+		if (audioClipArray.Length > 0)
+			return audioClipArray[Random.Range (0, audioClipArray.Length)];
+		return null;
+	}
+}
